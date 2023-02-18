46fc5e39cc113612f6c271424e1b6338e9119c86
Aftabul Islam <toaihimel@gmail.com>
2023-02-19 00:03:20 +0600

    property_group.title validation added. feature/initial-data-api

---
+++ app/tests/Unit/Http/Requests/PropertyGroupRequestTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit\Http\Requests;
+
+use App\Models\Property;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PropertyGroupRequestTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    protected string $routePrefix = 'api.property_groups.';
+
+    /** @test */
+    public function title_is_required()
+    {
+        $validationField = 'title';
+        $brokenRule = null;
+        $property = Property::factory()->make([
+            $validationField => $brokenRule
+        ]);
+
+        $this->postJson(
+            route($this->routePrefix . 'store'),
+            $property->toArray()
+        )->assertJsonValidationErrors($validationField);
+        $this->withoutExceptionHandling();
+    }
+
+    /** @test */
+    public function title_must_not_exceed_256_characters()
+    {
+        $validationField = 'title';
+        $brokenRule = $this->faker->words(259);
+        $property = Property::factory()->make([
+            $validationField => $brokenRule
+        ]);
+
+        $this->postJson(
+            route($this->routePrefix . 'store'),
+            $property->toArray()
+        )->assertJsonValidationErrors($validationField);
+        $this->withoutExceptionHandling();
+
+    }
+
+}
